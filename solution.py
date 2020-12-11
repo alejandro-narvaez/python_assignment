ff7172d99ceb17f1e82848a597182a5758e5ff1f
alejandro-narvaez <75843341+alejandro-narvaez@users.noreply.github.com>
2020-12-11 08:34:12 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -10,19 +10,19 @@ def welcome_assignment_answers(question):
     if question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "Yes"
-    elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
-        answer = (hashlib.md5(b'NYU Computer Networking').digest())
+        answer = (hashlib.md5(b'NYU Computer Networking').hexdigest())
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = 5
+        answer = int("5")
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = 4
+        answer = int("4")
     return(answer)
 
 # Complete all the questions.
